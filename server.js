a04db4eeab5814c8400d421974d70edcf3e39a91
jitsvz <50335437+jitsvz@users.noreply.github.com>
2023-03-18 15:57:08 +0100

    fix server 2

--- server.js
+++ server.js
@@ -1,9 +1,34 @@
-const http = require("http")
+const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser')
 
-function handleRequest(req, res) {
-    console.log(req.url, req.headers, req.method)
-}
+const db = require('./db.json')
 
-const server = http.createServer(handleRequest)
+const app = express();
 
-server.listen(3003)
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+//ROUTES
+//RESTFul API
+
+app.get('/games', (req, res, next) => {
+    const games = db.games
+    res.type('json')
+    res.send(JSON.stringify(games));
+    // res.json(games)
+});
+
+app.post('/games', (req, res, next) => {
+    const game = req.body
+    db.games.push(game)
+    res.send(JSON.stringify(game))
+})
+
+app.use('/home', (req, res, next) => {
+    res.send('<form action="/games" method="POST"><input type="input" name="name"><input type="input" name="category"><input type="input" name="price"><button type="submit">voeg spel toe!</button></form>');
+});
+
+const server = http.createServer(app);
+
+server.listen(3003);
