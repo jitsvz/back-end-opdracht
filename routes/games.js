41b163cb17e1a2622c28a9a71a8296951cf60e0a
jitsvz <50335437+jitsvz@users.noreply.github.com>
2023-03-22 08:02:01 +0100

    changed routes

--- routes/games.js
+++ routes/games.js
@@ -10,7 +10,7 @@ router.get('/games/:id', (req, res, next) => {
     const id = parseInt(req.params.id)
     let game = db.games.find(g => g.id == id)
     let responseCode = 404
-    if(games){
+    if (games) {
         responseCode = 200
     } else {
         game = {}
@@ -20,10 +20,10 @@ router.get('/games/:id', (req, res, next) => {
 
 router.delete('/games/:id', (req, res, next) => {
     const id = parseInt(req.params.id)
-    const index = db.games.findIndex( g => g.id == id)
+    const index = db.games.findIndex(g => g.id == id)
     let deleteGame = {}
     let responseCode = 404
-    if(index > -1){
+    if (index > -1) {
         deleteGame = db.games.splice(index, 1)[0]
         responseCode = 200
     }
@@ -34,52 +34,44 @@ router.put('/games/:id', (req, res, next) => {
     const id = parseInt(req.params.id)
     const game = req.body
     let responseCode = 200
-    const index = db.games.findIndex( g => g.id == id)
-    if(index > -1){
-        db.games.splice(index,1)
+    const index = db.games.findIndex(g => g.id == id)
+    if (index > -1) {
+        db.games.splice(index, 1)
     }
     game.id = id
     db.games.push(game)
     res.status(responseCode).json(games)
 });
 
-router.patch('/games/:id', (req, res, next) => {
-    const id = parseInt(req.params.id)
-    const game = req.body
-    const index = db.games.findIndex( G => G.id == id)
-    let foundGame ={}
-    let responsecode = 404
-    if(index > -1) {
-        foundGame = db.games.splice(index, 1)[0]
-        foundGame = {...foundGame,...game}
-        db.games.push(foundGame)
-        responseCode = 200
-    }
-    res.status(responseCode).json(foundGame)
-});
-
 router.get('/games', (req, res, next) => {
-    let games = []
+    games = db.games
     let responseCode = 200
-
-    if(req.query.price){
-        const filterFor = parseFloat(req.query.price)
-        let responseCode = 200
-        games = db.games.filter( g => g.price <= filterFor ) || {}
-    } else {
-        let responseCode = 404
-        games = db.games
-    }   
     res.status(responseCode).json(games)
 });
 
 router.post('/games', (req, res, next) => {
-    const game = req.body
-    const games = db.games
-    db.games.push(game)
-    let responseCode = 200
+    const game = req.body;
 
-    res.status(responseCode).json(games)
+    // Find the highest existing ID
+    let maxId = 0;
+    db.games.forEach((g) => {
+      if (g.id > maxId) {
+        maxId = g.id;
+      }
+    });
+  
+    // Generate a new unique ID
+    const newId = maxId + 1;
+    while (db.games.find((g) => g.id === newId)) {
+      newId++;
+    }
+  
+    // Add the new game with the generated ID
+    game.id = newId;
+    db.games.push(game);
+  
+    let responseCode = 200;
+    res.status(responseCode).json(db.games);
 })
 
 module.exports = router
